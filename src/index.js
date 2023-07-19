fd0dcb251b774f6c91637b89dead65da1359fb70
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-07-19 17:09:24 +0300

    completed assignemnts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from "react";
+ import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+function Parks() {
+  return (
+    <div>
+       <ColoradoStateParks />
+       <p>There are 42 parks in Colorado State Parks</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<Parks />, document.getElementById("root"));
